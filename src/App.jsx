9f49290f9062026533e45160e710bb878a24cf6e
SanjaysudhanSivashunmugam <sanjaysudhan.sivashunmugam@gmail.com>
2025-06-06 00:48:09 +0530

    add link in nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
-import About from "./components/About"
-import Footer from "./components/Footer"
-import Hero from "./components/Hero"
-import Info from "./components/Info"
-import Navbar from "./components/Navbar"
-import Project from "./components/Project"
-import Skills from "./components/Skills"
-
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Info from "./components/Info";
+import Navbar from "./components/Navbar";
+import Project from "./components/Project";
+import Skills from "./components/Skills";
 
 function App() {
-  
-
   return (
-   <div>
-    <Navbar />
-    <Hero />
-    <About />
-    <Skills />
-    <Project />
-    <Info />
-    <Footer />
-   </div>
-  )
+    <div>
+      <Navbar />
+      <section id="home">
+        <Hero />
+      </section>
+      <section id="about">
+        <About />
+      </section>
+      <section id="skills">
+        <Skills />
+      </section>
+      <section id="project">
+        <Project />
+      </section>
+      <section id="contact">
+        <Info />
+      </section>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,11 +16,11 @@ export default function Hero() {
                             <h1>Sanjaysudhan Sivashunmugam</h1>
                             <h5 className='mt-2'>Aspiring Software Developer</h5>
                             <div className="d-flex gap-3 mt-3 mb-5">
-                                <a href="" className='btn btn-light rounded-circle'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="30" fill="currentColor" class="bi bi-person-rolodex" viewBox="0 0 16 16">
+                                <a href="#contact" className='btn btn-light rounded-circle'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="30" fill="currentColor" class="bi bi-person-rolodex" viewBox="0 0 16 16">
                                     <path d="M8 9.05a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5" />
                                     <path d="M1 1a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h.5a.5.5 0 0 0 .5-.5.5.5 0 0 1 1 0 .5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5.5.5 0 0 1 1 0 .5.5 0 0 0 .5.5h.5a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H6.707L6 1.293A1 1 0 0 0 5.293 1zm0 1h4.293L6 2.707A1 1 0 0 0 6.707 3H15v10h-.085a1.5 1.5 0 0 0-2.4-.63C11.885 11.223 10.554 10 8 10c-2.555 0-3.886 1.224-4.514 2.37a1.5 1.5 0 0 0-2.4.63H1z" />
                                 </svg></a>
-                                <a href="" className='btn btn-light rounded-circle'>
+                                <a href="https://github.com/SanjaysudhanSivashunmugam" className='btn btn-light rounded-circle'>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="30" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
                                         <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
                                     </svg>
